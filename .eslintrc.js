4166a37c504dffb40b34a25e24f658a5b680787a
jasonm00n <joffrey.berrier@gmail.com>
2018-11-04 20:56:43 +0100

    update readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,13 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'on',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'on',
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
+  rules: {
+    'max-len': ["error", { "code": 120 }]
+  }
 };
--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@ export default {
           },
           name: {
             type: 'input',
-            value: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
+            value: 'John',
             active: false,
             style: {
               color: '#000',
