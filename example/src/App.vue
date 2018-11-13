45decb5c253cf8172399993314e4afee7f5e5991
Joffrey <joffrey.berrier@gmail.com>
2018-11-13 10:31:50 +0100

    publish on npm, create an example folder

---
+++ example/src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+    <vue-table
+      :data="products"
+      :headers="headers"
+      :drag-to-fill="dragToFill"
+      :submenu-tbody="submenuTbody"
+      :submenu-thead="submenuThead"
+      v-on:thead-submenu-click-change-color="changeColor"
+      v-on:thead-submenu-click-change-value="changeValue"
+      v-on:thead-submenu-click-change-city="changeCity"
+      v-on:tbody-submenu-click-change-color="changeColorTbody"
+      v-on:tbody-submenu-click-change-value="changeValueTbody"
+      v-on:tbody-input-change="inputChange"
+      v-on:tbody-select-change="selectChange">
+    </vue-table>
+  </div>
+</template>
+
+<script>
+import VueTable from 'spreadsheet-vuejs';
+import exempleData from './data';
+
+export default {
+  name: 'app',
+  data() {
+    return exempleData;
+  },
+  components: {
+    VueTable,
+  },
+  methods: {
+    inputChange(event, entry, rowIndex, colIndex) {
+      console.log('InputChange', event, entry, rowIndex, colIndex);
+    },
+    selectChange(event, entry, rowIndex, colIndex) {
+      // console.log('selectChange', event, entry, rowIndex, colIndex);
+      this.changeValueSelect(rowIndex, colIndex);
+    },
+    // callback
+    changeCity(event, entry, colIndex, selectOptions) {
+      this.products.forEach((elm) => {
+        const product = elm;
+        product[entry].value = selectOptions;
+      });
+    },
+    changeValueSelect(rowIndex, colIndex) {
+      console.log('changeValueSelect', rowIndex, colIndex);
+      // to get our element => Object.values(this.products[rowIndex])[colIndex];
+    },
+    changeColor(event, entry, colIndex) {
+      // console.log('changeColor', event, entry, colIndex);
+      this.headers[colIndex].style.color = '#e40000';
+    },
+    changeValue(event, entry, colIndex) {
+      // console.log('changeValue', event, entry, colIndex);
+      this.headers[colIndex].headerName = 'T-shirt';
+    },
+    changeColorTbody(event, entry, rowIndex, colIndex, type) {
+      // console.log('changeColorTbody', event, entry, rowIndex, colIndex, type);
+      if (type === 'input') {
+        this.products[rowIndex][entry].style.color = '#e40000';
+      }
+    },
+    changeValueTbody(event, entry, rowIndex, colIndex, type) {
+      // console.log('changeValueTbody', event, entry, rowIndex, colIndex, type);
+      if (type === 'input') {
+        this.products[rowIndex][entry].value = 'T-shirt';
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  color: #2c3e50;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: #fff;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font: 400 11px system-ui;
+}
+::-moz-selection { /* Code for Firefox */
+  color: #2c3e50;
+  background: transparent;
+}
+::selection {
+  color: #2c3e50;
+  background: transparent;
+}
+.row,
+.thead {
+  display: flex;
+}
+.wrap {
+  margin: 10px auto;
+}
+</style>
---
+++ example/src/data.js
@@ -0,0 +1,413 @@
+export default {
+  dragToFill: true,
+  headers: [
+    {
+      headerName: 'Image',
+      headerKey: 'img',
+      style: {
+        color: '#000',
+      },
+    },
+    {
+      headerName: 'Nom',
+      headerKey: 'name',
+      style: {
+        color: '#000',
+      },
+    },
+    {
+      headerName: 'Prénom',
+      headerKey: 'surname',
+      style: {
+        color: '#000',
+      },
+    },
+    {
+      headerName: 'Age',
+      headerKey: 'age',
+      style: {
+        color: '#000',
+      },
+    },
+    {
+      headerName: 'Born',
+      headerKey: 'born',
+      style: {
+        color: '#000',
+      },
+    },
+  ],
+  products: [
+    {
+      img: {
+        type: 'img',
+        value: 'https://via.placeholder.com/350x150',
+        active: false,
+        disabled: true,
+      },
+      name: {
+        type: 'input',
+        value: 'John',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      surname: {
+        type: 'input',
+        value: 'Doe',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      age: {
+        type: 'select',
+        handleSearch: true,
+        selectOptions: [
+          {
+            value: 26,
+            label: 26,
+          },
+          {
+            value: 27,
+            label: 27,
+          },
+          {
+            value: 28,
+            label: 28,
+          },
+          {
+            value: 29,
+            label: 29,
+          },
+          {
+            value: 30,
+            label: 30,
+          },
+        ],
+        value: 28,
+        active: false,
+      },
+      born: {
+        type: 'select',
+        handleSearch: true,
+        selectOptions: [
+          {
+            value: 1980,
+            label: 1980,
+          },
+          {
+            value: 1981,
+            label: 1981,
+          },
+          {
+            value: 1982,
+            label: 1982,
+          },
+          {
+            value: 1983,
+            label: 1983,
+          },
+          {
+            value: 1984,
+            label: 1984,
+          },
+        ],
+        value: 1990,
+        active: false,
+      },
+    },
+    {
+      img: {
+        type: 'img',
+        value: 'https://via.placeholder.com/350x150',
+        active: false,
+        disabled: false,
+      },
+      name: {
+        type: 'input',
+        value: 'Jane',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      surname: {
+        type: 'input',
+        value: 'Boe',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      age: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 26,
+            label: 26,
+          },
+          {
+            value: 27,
+            label: 27,
+          },
+          {
+            value: 28,
+            label: 28,
+          },
+          {
+            value: 29,
+            label: 29,
+          },
+          {
+            value: 30,
+            label: 30,
+          },
+        ],
+        value: 26,
+        active: false,
+      },
+      born: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 1980,
+            label: 1980,
+          },
+          {
+            value: 1981,
+            label: 1981,
+          },
+          {
+            value: 1982,
+            label: 1982,
+          },
+          {
+            value: 1983,
+            label: 1983,
+          },
+          {
+            value: 1984,
+            label: 1984,
+          },
+        ],
+        value: 1990,
+        active: false,
+      },
+    },
+    {
+      img: {
+        type: 'img',
+        value: 'https://via.placeholder.com/350x150',
+        active: false,
+        disabled: false,
+      },
+      name: {
+        type: 'input',
+        value: 'Jim',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      surname: {
+        type: 'input',
+        value: 'Die',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      age: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 26,
+            label: 26,
+          },
+          {
+            value: 27,
+            label: 27,
+          },
+          {
+            value: 28,
+            label: 28,
+          },
+          {
+            value: 29,
+            label: 29,
+          },
+          {
+            value: 30,
+            label: 30,
+          },
+        ],
+        value: 30,
+        active: false,
+      },
+      born: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 1980,
+            label: 1980,
+          },
+          {
+            value: 1981,
+            label: 1981,
+          },
+          {
+            value: 1982,
+            label: 1982,
+          },
+          {
+            value: 1983,
+            label: 1983,
+          },
+          {
+            value: 1984,
+            label: 1984,
+          },
+        ],
+        value: 1990,
+        active: false,
+      },
+    },
+    {
+      img: {
+        type: 'img',
+        value: 'https://via.placeholder.com/350x150',
+        active: false,
+        disabled: false,
+      },
+      name: {
+        type: 'input',
+        value: 'Jam',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      surname: {
+        type: 'input',
+        value: 'Dam',
+        active: false,
+        style: {
+          color: '#000',
+        },
+      },
+      age: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 26,
+            label: 26,
+          },
+          {
+            value: 27,
+            label: 27,
+          },
+          {
+            value: 28,
+            label: 28,
+          },
+          {
+            value: 29,
+            label: 29,
+          },
+          {
+            value: 30,
+            label: 30,
+          },
+        ],
+        value: 28,
+        active: false,
+      },
+      born: {
+        type: 'select',
+        handleSearch: false,
+        selectOptions: [
+          {
+            value: 1980,
+            label: 1980,
+          },
+          {
+            value: 1981,
+            label: 1981,
+          },
+          {
+            value: 1982,
+            label: 1982,
+          },
+          {
+            value: 1983,
+            label: 1983,
+          },
+          {
+            value: 1984,
+            label: 1984,
+          },
+        ],
+        value: 1990,
+        active: false,
+      },
+    },
+  ],
+  submenuThead: [
+    {
+      type: 'button',
+      value: 'change color',
+      function: 'change-color',
+      disabled: ['img'],
+    },
+    {
+      type: 'select',
+      disabled: ['img'],
+      subtitle: 'Select state:',
+      selectOptions: [
+        {
+          value: 'new-york',
+          label: 'new-york',
+        },
+        {
+          value: 'france',
+          label: 'france',
+        },
+      ],
+      value: 'new-york',
+      buttonOption: {
+        value: 'change city',
+        function: 'change-city',
+        style: {
+          display: 'block',
+        },
+      },
+    },
+    {
+      type: 'button',
+      value: 'change value',
+      function: 'change-value',
+      disabled: ['img', 'name'],
+    },
+  ],
+  submenuTbody: [
+    {
+      type: 'button',
+      value: 'change color',
+      function: 'change-color',
+      disabled: ['img'],
+    },
+    {
+      type: 'button',
+      value: 'change value',
+      function: 'change-value',
+      disabled: ['img', 'name'],
+    },
+  ],
+};
