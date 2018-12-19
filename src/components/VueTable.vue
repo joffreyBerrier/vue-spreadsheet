61b125363f6ad2db0f4f949cc6df02509fd6245a
Joffrey <joffrey.berrier@gmail.com>
2018-12-19 18:27:40 +0100

    store old rectangleSelection and clean styleSetAttribute when copy or paste

--- src/components/VueTable.vue
+++ src/components/VueTable.vue
@@ -136,6 +136,8 @@ export default {
       oldTdActive: null,
       oldTdShow: null,
       pressedShift: 0,
+      rectangleSelectedCell: null,
+      storeRectangleSelection: [],
       scrollDocument: null,
       scrollToSelectTimeout: null,
       selectedCell: null,
@@ -177,6 +179,23 @@ export default {
     },
   },
   methods: {
+    cleanPropertyOnCell(action) {
+      if (this.storeRectangleSelection.length > 0) {
+        this.storeRectangleSelection.forEach((cell) => {
+          if (action === 'paste' && !cell.classList.value.includes('rectangleSelection') && !cell.classList.value.includes('copy')) {
+            this.cleanProperty(cell);
+          } else if (action === 'copy' && !cell.classList.value.includes('selected')) {
+            this.cleanProperty(cell);
+          }
+        });
+      }
+    },
+    cleanProperty(element) {
+      element.style.setProperty('--rectangleWidth', '100%');
+      element.style.setProperty('--rectangleHeight', '40px');
+      element.style.setProperty('--rectangleTop', 0);
+      element.style.setProperty('--rectangleBottom', 0);
+    },
     createdCell() {
       // create cell if isn't exist
       this.tbodyData.forEach((tbody, rowIndex) => {
@@ -440,6 +459,7 @@ export default {
         if (params === 'copy') {
           this.$set(this.tbodyData[rowMin][header], 'stateCopy', true);
           this.removeClass(['rectangleSelection']);
+          this.cleanPropertyOnCell('copy');
         }
 
         while (rowMin <= rowMax) {
@@ -455,9 +475,10 @@ export default {
         this.copyMultipleCell = true;
       } else {
         if (params === 'copy') {
+          this.cleanPropertyOnCell('copy');
           this.$set(this.tbodyData[this.selectedCell.row][this.selectedCell.header], 'stateCopy', true);
         } else {
-          this.storeCopyDatas = [];  
+          this.storeCopyDatas = [];
         }
         this.storeCopyDatas.push(tbodyData[this.selectedCell.row][this.selectedCell.header]);
         this.copyMultipleCell = false;
@@ -465,6 +486,8 @@ export default {
     },
     pasteReplaceData() {
       const maxRow = this.tbodyData.length;
+      this.cleanPropertyOnCell('paste');
+
       // copy / paste one cell || disable on disabled cell
       if (this.storeCopyDatas[0].value && !this.copyMultipleCell && !this.selectedMultipleCell && !this.eventDrag && this.disabledEvent(this.selectedCell.col, this.selectedCell.header)) {
         const newCopyData = JSON.parse(JSON.stringify(this.storeCopyDatas));
@@ -624,29 +647,33 @@ export default {
       this.$forceUpdate();
     },
     setRectangleSelection(width, height) {
-      let rectangleSelectedCell = this.$refs.vueTbody.$refs[`td-${this.selectedCoordCells.colStart}-${this.selectedCoordCells.rowStart}`][0];
+      this.rectangleSelectedCell = this.$refs.vueTbody.$refs[`td-${this.selectedCoordCells.colStart}-${this.selectedCoordCells.rowStart}`][0];
 
       if (!this.selectedMultipleCellActive) {
-        rectangleSelectedCell = this.$refs.vueTbody.$refs[`td-${this.selectedCell.col}-${this.selectedCell.row}`][0];
+        this.rectangleSelectedCell = this.$refs.vueTbody.$refs[`td-${this.selectedCell.col}-${this.selectedCell.row}`][0];
       }
 
-      rectangleSelectedCell.style.setProperty('--rectangleWidth', `${width + 1}%`);
-      rectangleSelectedCell.style.setProperty('--rectangleHeight', `${height}px`);
+      this.rectangleSelectedCell.style.setProperty('--rectangleWidth', `${width + 1}%`);
+      this.rectangleSelectedCell.style.setProperty('--rectangleHeight', `${height}px`);
 
       // Position bottom/top of rectangle if rowStart >= rowEnd
       if (this.selectedCoordCells.rowStart >= this.selectedCoordCells.rowEnd) {
-        rectangleSelectedCell.style.setProperty('--rectangleTop', 'auto');
-        rectangleSelectedCell.style.setProperty('--rectangleBottom', 0);
+        this.rectangleSelectedCell.style.setProperty('--rectangleTop', 'auto');
+        this.rectangleSelectedCell.style.setProperty('--rectangleBottom', 0);
       } else {
-        rectangleSelectedCell.style.setProperty('--rectangleTop', 0);
-        rectangleSelectedCell.style.setProperty('--rectangleBottom', 'auto');
+        this.rectangleSelectedCell.style.setProperty('--rectangleTop', 0);
+        this.rectangleSelectedCell.style.setProperty('--rectangleBottom', 'auto');
       }
       // Position left/right of rectangle if colStart >= colEnd
       if (this.selectedCoordCells.colStart >= this.selectedCoordCells.colEnd) {
-        rectangleSelectedCell.style.setProperty('--rectangleLeft', 'auto');
-        rectangleSelectedCell.style.setProperty('--rectangleRight', 0);
+        this.rectangleSelectedCell.style.setProperty('--rectangleLeft', 'auto');
+        this.rectangleSelectedCell.style.setProperty('--rectangleRight', 0);
       } else {
-        rectangleSelectedCell.style.setProperty('--rectangleLeft', 0);
+        this.rectangleSelectedCell.style.setProperty('--rectangleLeft', 0);
+      }
+
+      if (!this.storeRectangleSelection.includes(this.rectangleSelectedCell)) {
+        this.storeRectangleSelection.push(this.rectangleSelectedCell);
       }
     },
     // drag To Fill
