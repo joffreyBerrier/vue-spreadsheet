fc19693b8c23326bda35a5348c007130445a9f7b
Joffrey <joffrey.berrier@gmail.com>
2019-01-15 12:15:01 +0100

    write test for vueTable data / methods

--- test/VueTable.spec.js
+++ test/VueTable.spec.js
@@ -51,6 +51,37 @@ describe('VueTable', () => {
     });
   });
 
+  describe('Data', () => {
+    test('Present Data', () => {
+      const vueTable = wrapper.vm;
+
+      expect(vueTable.disableKeyTimeout).toEqual(null);
+      expect(vueTable.eventDrag).toEqual(false);
+      expect(vueTable.incrementCol).toEqual(0);
+      expect(vueTable.incrementOption).toEqual(null);
+      expect(vueTable.incrementRow).toEqual(null);
+      expect(vueTable.keys).toEqual({});
+      expect(vueTable.lastSelectOpen).toEqual(null);
+      expect(vueTable.lastSubmenuOpen).toEqual(null);
+      expect(vueTable.oldTdActive).toEqual(null);
+      expect(vueTable.oldTdShow).toEqual(null);
+      expect(vueTable.pressedShift).toEqual(0);
+      expect(vueTable.rectangleSelectedCell).toEqual(null);
+      expect(vueTable.scrollDocument).toEqual(null);
+      expect(vueTable.scrollToSelectTimeout).toEqual(null);
+      expect(vueTable.selectedCell).toEqual(null);
+      expect(vueTable.selectedCoordCells).toEqual(null);
+      expect(vueTable.selectedCoordCopyCells).toEqual(null);
+      expect(vueTable.selectedMultipleCell).toEqual(false);
+      expect(vueTable.selectedMultipleCellActive).toEqual(false);
+      expect(vueTable.setFirstCell).toEqual(false);
+      expect(vueTable.storeCopyDatas).toEqual([]);
+      expect(vueTable.storeRectangleSelection).toEqual([]);
+      expect(vueTable.submenuStatusTbody).toEqual(false);
+      expect(vueTable.submenuStatusThead).toEqual(false);
+    });
+  });
+  
   describe('Computed', () => {
 
     test('colHeaderWidths', () => {
@@ -89,5 +120,177 @@ describe('VueTable', () => {
       const headerKeysTest = wrapper.vm.headers.map(header => header.headerKey);
       expect(headerKeys).toEqual(headerKeysTest);
     });
+  });
+
+  describe('Methods', () => {
+    describe('DisabledEvent', () => {
+      test('Disabled Col : false | with disableCells', () => {
+        const fakeData = { disabled: false };
+        expect(wrapper.vm.disabledEvent(fakeData, 'a')).toBeTruthy()
+      })
+      test('Disabled Col : true | with disableCells', () => {
+        const fakeData = { disabled: true };
+        expect(wrapper.vm.disabledEvent(fakeData, 'a')).toBeFalsy();
+      });
+      test('Disabled Col : false | without disableCells', () => {
+        const fakeData = { disabled: false };
+        expect(wrapper.vm.disabledEvent(fakeData, 'b')).toBeTruthy();
+      });
+      test('Disabled Col : true | without disableCells', () => {
+        const fakeData = { disabled: true };
+        expect(wrapper.vm.disabledEvent(fakeData, 'b')).toBeFalsy();
+      });
+    });
+
+    describe('enableSubmenu', () => {
+      test('params Thead', () => {
+        wrapper.vm.submenuStatusThead = false;
+        wrapper.vm.submenuStatusTbody = true;
+        wrapper.vm.enableSubmenu('thead');
+        expect(wrapper.vm.submenuStatusThead).toBeTruthy();
+        expect(wrapper.vm.submenuStatusTbody).toBeFalsy();
+      });
+
+      test('params Tbody', () => {
+        wrapper.vm.submenuStatusThead = true;
+        wrapper.vm.submenuStatusTbody = false;
+        wrapper.vm.enableSubmenu('tbody');
+        expect(wrapper.vm.submenuStatusThead).toBeFalsy();
+        expect(wrapper.vm.submenuStatusTbody).toBeTruthy();
+      });
+
+      test('no Params', () => {
+        wrapper.vm.submenuStatusThead = true;
+        wrapper.vm.submenuStatusTbody = true;
+        wrapper.vm.enableSubmenu();
+        expect(wrapper.vm.submenuStatusThead).toBeFalsy();
+        expect(wrapper.vm.submenuStatusTbody).toBeFalsy();
+      });
+    });
+
+    describe('bindClassActiveOnTd', () => {
+      const row = 0;
+      const col = 0;
+      const header = 'a';
+
+      test('show => be false | active => be truthly', () => {
+        wrapper.vm.tbodyData[row][header].show = true;
+        wrapper.vm.tbodyData[row][header].active = false;
+        wrapper.vm.bindClassActiveOnTd(header, row, col);
+        expect(wrapper.vm.tbodyData[row][header].show).toBeFalsy();
+        expect(wrapper.vm.tbodyData[row][header].active).toBeTruthy();
+      });
+      test('oldTdActive equal to actualTd', () => {
+        wrapper.vm.bindClassActiveOnTd(header, row, col);
+        expect(wrapper.vm.oldTdActive.key).toEqual(header);
+        expect(wrapper.vm.oldTdActive.row).toEqual(row);
+        expect(wrapper.vm.oldTdActive.col).toEqual(col);
+      });
+    });
+
+    describe('RemoveClass', () => {
+      test('Keys to be falsy', () => {
+        const fakeParams = ['selected', 'rectangleSelection', 'active', 'show', 'search', 'typing', 'stateCopy'];
+        // Add keys to true
+        Object.values(wrapper.vm.tbodyData[0]).forEach(tbodyData => {
+          const data = tbodyData;
+          data.selected = true;
+          data.rectangleSelection = true;
+          data.active = true;
+          data.show = true;
+          data.search = true;
+          data.typing = true;
+          data.stateCopy = true;
+        });
+
+        wrapper.vm.removeClass(fakeParams);
+
+        // Expect keys are false
+        Object.values(wrapper.vm.tbodyData[0]).forEach(tbodyData => {
+          const data = tbodyData;
+          expect(data.selected).toBeFalsy();
+          expect(data.rectangleSelection).toBeFalsy();
+          expect(data.active).toBeFalsy();
+          expect(data.show).toBeFalsy();
+          expect(data.search).toBeFalsy();
+          expect(data.typing).toBeFalsy();
+          expect(data.stateCopy).toBeFalsy();
+        });
+      });
+
+      test('selectedMultipleCellActive to be falsy', () => {
+        wrapper.vm.selectedMultipleCellActive = true;
+        wrapper.vm.removeClass(['selected']);
+        expect(wrapper.vm.selectedMultipleCellActive).toBeFalsy();
+      });
+
+      test('rectangleSelection to be falsy', () => {
+        wrapper.vm.setFirstCell = true;
+        wrapper.vm.removeClass(['rectangleSelection']);
+        expect(wrapper.vm.setFirstCell).toBeFalsy();
+      });
+    });
+
+    describe('updateSelectedCell', () => {
+      const row = 0;
+      const col = 0;
+      const header = 'a';
+
+      test('Check selectedCell', () => {
+        wrapper.vm.updateSelectedCell(header, row, col);
+        expect(wrapper.vm.selectedCell.header).toEqual(header);
+        expect(wrapper.vm.selectedCell.row).toEqual(row);
+        expect(wrapper.vm.selectedCell.col).toEqual(col);
+      });
+
+      test('setFirstCell = false', () => {
+        wrapper.vm.setFirstCell = false;
+        wrapper.vm.updateSelectedCell(header, row, col);
+        expect(wrapper.vm.setFirstCell).toBeTruthy();
+        expect(wrapper.vm.tbodyData[row][header].rectangleSelection).toBeTruthy()
+      });
+      
+      test('setFirstCell = true', () => {
+        wrapper.vm.setFirstCell = true;
+        wrapper.vm.updateSelectedCell(header, row, col);
+        expect(wrapper.vm.setFirstCell).toBeTruthy();
+        expect(wrapper.vm.tbodyData[row][header].rectangleSelection).toBeTruthy();
+      });
+    });
+
+    describe('enableSelect', () => {
+      const rowIndex = 0;
+      const colIndex = 0;
+      const header = 'a';
+
+      test('search true', () => {
+        const data = {
+          search: true,
+        };
+
+        wrapper.vm.enableSelect('', header, data, rowIndex, colIndex);
+        expect(wrapper.vm.tbodyData[rowIndex][header].search).toBeFalsy();
+        expect(wrapper.vm.tbodyData[rowIndex][header].show).toBeFalsy();
+        expect(wrapper.vm.tbodyData[rowIndex][header].typing).toBeTruthy();
+        expect(wrapper.vm.lastSelectOpen).toBeNull();
+      });
+
+      // test('search false', () => {
+      //   const data = {
+      //     search: false,
+      //   };
+
+      //   wrapper.vm.tbodyData[rowIndex][header].search = true;
+      //   wrapper.vm.tbodyData[rowIndex][header].show = true;
+      //   wrapper.vm.tbodyData[rowIndex][header].typing = true;
+      //   wrapper.vm.enableSelect('', header, data, rowIndex, colIndex);
+
+      //   // expect(wrapper.vm.tbodyData[rowIndex][header].search).toBeTruthy();
+      //   // expect(wrapper.vm.tbodyData[rowIndex][header].show).toBeTruthy();
+      //   // expect(wrapper.vm.tbodyData[rowIndex][header].typing).toBeFalsy();
+      //   // expect(wrapper.vm.lastSelectOpen).toBeNull();
+      // });
+    });
+
   });
 });
